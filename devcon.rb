f45ca0883deddd16c650eea1641d99a5cbe32d42
kreemer <kevin@familie-studer.ch>
2026-02-19 08:34:36 +0100

    chore: release new version 0.2.9

--- devcon.rb
+++ devcon.rb
@@ -1,30 +1,30 @@
 class Devcon < Formula
   desc "DevCon One - Your Mission-Critical Dev Environment Manager"
   homepage "https://github.com/devconhq/devcon"
-  version "0.2.8"
+  version "0.2.9"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/devconhq/devcon/releases/download/v0.2.8/devcon-macos-arm64"
-      sha256 "cec92256e17f6495de5022eb4d9775d5f4c114c79e483e46e8f31637dd8bf444"
+      url "https://github.com/devconhq/devcon/releases/download/v0.2.9/devcon-macos-arm64"
+      sha256 "sha256:9314fe7cc40461bde5d5bf02349d1493c177334d0329bf9baf4bff1b9e00f63d"
     end
 
     on_intel do
-      url "https://github.com/devconhq/devcon/releases/download/v0.2.8/devcon-macos-universal"
-      sha256 "1dc6c98eefc2bb21b86bed92c83d0604b48d9e04ea27d6763a25c5badf99931a"
+      url "https://github.com/devconhq/devcon/releases/download/v0.2.9/devcon-macos-universal"
+      sha256 "sha256:d3ff9def2b3399caa259b419f2bddc832f062d25ed6bfb9568a7c93b7ec1fda5"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/devconhq/devcon/releases/download/v0.2.8/devcon-linux-x86_64"
-      sha256 "08c62626609dd8d752b74fba5e1fd243e40f1cc71061f5e75f2cf307b11964ef"
+      url "https://github.com/devconhq/devcon/releases/download/v0.2.9/devcon-linux-x86_64"
+      sha256 "sha256:3d70618ffa57c18c7ea0b5c72e88708e410f27bcced062a7806111a9307a90f8"
     end
 
     on_arm do
-      url "https://github.com/devconhq/devcon/releases/download/v0.2.8/devcon-linux-arm64"
-      sha256 "89f2dc794096131a363761bd9d3b45b8bdf03c5b96aa9bc046773b4498bf5dc4"
+      url "https://github.com/devconhq/devcon/releases/download/v0.2.9/devcon-linux-arm64"
+      sha256 "sha256:fb634aade9a4a448b73ed5914589f6437d506fb481a80fd67c3974b2b626ac29"
     end
   end
 
